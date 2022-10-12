5f13fe2eea600eaaab93046b6073332c974df481
CarolinaIPD <delgadoisabel571@gmail.com>
2022-10-11 22:46:05 -0600

    Preguntas y terminos

---
+++ wordpress/wp-content/plugins/tidio-live-chat/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd8446490b4af1434cd77875ceb747ccbfb7ddc5e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd8446490b4af1434cd77875ceb747ccbfb7ddc5e',
+    ),
+  ),
+);
